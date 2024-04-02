a353230ce30d7ef2448ef65869e9fc5d342174ef
zelmano <zelman.brun@gmail.com>
2024-04-02 13:10:43 +0200

    new navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Navbar({}: Props) {
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">Navbar</nav>
+  );
+}
